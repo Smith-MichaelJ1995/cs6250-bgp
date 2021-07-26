88899d5e2d795c1159229cb3057e3e700efd0e21
Michael Smith <michaelsmith1388@yahoo.com>
2021-07-26 03:34:00 -0400

    task 3 graph done-ish

--- task3.py
+++ task3.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import json
 from matplotlib import pyplot as plt
+from statsmodels.distributions.empirical_distribution import ECDF
 
 
 # read in files from filesystem
@@ -15,157 +16,109 @@ def write_to_filesystem(results):
 	with open('task2-results.json', 'w') as fp:
 		json.dump(results, fp)
 		
-
-def generate_results():
+def generate_task3_results():
 
    # Replace the contents of this cell with your code for Task 1 part A
-   results = {}
    notebook_path = os.path.abspath("task3.py")
    update_files_blackholing_directory_path = os.path.join(os.path.dirname(notebook_path), "update_files_blackholing")
    update_files = os.listdir(update_files_blackholing_directory_path)
    
-   #print(update_files)
-   #exit()
+   # prefix placeholders with IPV4 vuln
+   vulnPrefixes = {}
    
    # iterate through all rib files, create PyStream events
    for update_file in update_files:
-   
-      #print("Processing New File: {}".format(rib_file))
     	
       # create stream object with provided file
       epoch = update_file.split(".")[3]
       stream = pybgpstream.BGPStream(data_interface="singlefile")
       stream.set_data_interface_option("singlefile", "upd-file", "{}/{}".format(update_files_blackholing_directory_path, update_file))
-      #results[epoch] = {
-      # "originAses": {}
-      #}
     	
       # process stream for prefixes & ASES
       for index, element in enumerate(stream):
-    		
-         print("processing index = {}".format(index))
-         print("processing element = {}".format(element))
-         exit()
-    			
-         # get the list of ASes in the AS path
-         ases = element.fields["as-path"].split(" ")
-    			
-         # get the origin ASn (rightmost)
-         origin = ases[-1]
-    			
-         # handle case of origin as being a set
-         if "{" in origin:
-         
-            #print("type of origin is a set")
-            #print("origin = {}".format(origin))
-         
-            # remove old set from ases
-            ases.remove(origin)
-    			
-            # remove paren's from origin string
-            origin = origin.strip("{")
-            origin = origin.strip("}")
-            
-            # split based on commas
-            origin = origin.split(",")
-    				
-            # append origins back to list
-            ases.extend(origin)
-            
-            # hold right-most origin value
-            origin = origin[-1]
-            
-            #print("re-formatted origin = {}".format(origin))
-            #print("re-formatted ases = {}".format(ases))
-            #print("")
-    				
-         # filter for unique ases
-         ases = list(set(ases))
-         
-         #print("###########")
-         #print("origin = {}".format(origin))		
-         #print("ases = {}".format(ases))
-         
-         # handle originAses if it's unique
-         if origin not in results[epoch]["originAses"].keys():
-         
-            # set default shortest path length for originAs
-            results[epoch]["originAses"][origin] = len(ases)
-            
-         else:
-         
-            # fetch current shortest path length for originAs
-            currentShortestPathLength = results[epoch]["originAses"][origin]
-            asesLength = len(ases)
-            
-            # determine if we've found a shorter path for this AS
-            if asesLength < currentShortestPathLength:
-            
-               # stage this value
-               results[epoch]["originAses"][origin] = asesLength 
+      
+         # determine element type
+         type = element.type
                
-   return results    
-	
-# invoke function, generate results, stage to filesystem	
-results = generate_results()
-# write_to_filesystem(results)
+         # determine time
+         time = element.time
+         
+         # handle based on event type
+         if type == "A":
+            
+            # get the prefix
+            pfx = element.fields["prefix"]
+         
+            # get the peer address
+            peerIP = element.peer_address
+            
+            # fetch communities for given record
+            if 'communities' in element.fields:
+          
+               # extract communities
+               communities = element.fields['communities']
+               
+               # does element belong to ipv4 blackhole community
+               if len(communities) > 0:
+               
+                  # create container for key
+                  key = "{}=={}".format(pfx, peerIP)
+               
+                  # append start time
+                  vulnPrefixes[key] = {"start": time}
+            
+         elif type == "W":
+         
+            # create container for key
+            key = "{}=={}".format(pfx, peerIP)
+                  
+            # handle if we're listening for this withdrawl
+            if key in vulnPrefixes.keys() and vulnPrefixes[key]["start"] < time and len(vulnPrefixes[key].keys()) == 1:
+               
+               # append start time
+               vulnPrefixes[key]["end"] = time
+               vulnPrefixes[key]["duration"] = vulnPrefixes[key]["end"] - vulnPrefixes[key]["start"]
+               
+            else:
+               pass
+            
+   
+   #break         
+   return vulnPrefixes   
+   
+   
+def filterForPrefixDurations(vulnPrefixes):
+
+   vulnPrefixDurations = []
+
+   # traverse through all vulns
+   for vulnPrefix in vulnPrefixes.keys():
+   
+     # check for empty start event
+     if len(vulnPrefixes[vulnPrefix].keys()) > 1:
+        vulnPrefixDurations.append(vulnPrefixes[vulnPrefix]["duration"]) 
+      
+   # return vulnerPrefixDurations
+   return vulnPrefixDurations  
+   
+
+# fetch results
+vulnPrefixes = generate_task3_results()
+
+# find durations
+durationData = filterForPrefixDurations(vulnPrefixes)
+
+   		
+# instantiate ecdf object
+ecdf = ECDF(durationData)
+   		
+# configure this plot data
+plt.plot(ecdf.x, ecdf.y, lw = 2)
+plt.title("ECDF of Blackhole Duration & Probability", size=14)
+plt.xlabel("Duration", size=14)
+plt.ylabel("Prob", size = 14)
    	
-		
-		
-		
-		
-		
-		
-		
-		
+# print plot
+plt.show()
 
-# fetch results dict from the filesystem
-#results = json.loads(read_from_filesystem())
 
-# traverse through all epochs in order
-#for epoch in sorted(results.keys()):
-
-   # fetch all unique originAses within this epoch
-#   asesInEpoch = results[epoch]['ases']
-   
-#   print("asesInEpoch = {}".format(asesInEpoch))
-#   exit()
-   
-   # return all ases for this
-#   for path in asesInEpoch:
-   
-      # extrapolate the originAs
-#      originAsInPath = path[-1]
-      
-      # extract prior originAs ID
-#     distinctOriginDataSet = results[epoch]["originAses"][originAsInPath]
-      
-      # determine if this originAs has been converted to a dict yet
-#       if isinstance(distinctOriginDataSet, list):
-      
-         # store dict in place of old integer value
-#         results[epoch]["originAses"][originAsInPath] = { 
-#            "uniqueOriginAsPrefixes": distinctOriginDataSet,
-#            "paths": [path]
-#         }
-         
-#      else:
-      
-#        print("originAsInPath = {}".format(originAsInPath))
-#         print(results[epoch]["originAses"][originAsInPath])
-#         exit()
-      
-         # place the new value
-#         results[epoch]["originAses"][originAsInPath]["paths"].append(path)
-         
-
-# traverse through all originAses to validate output
-#for uniqueAses in results[epoch]["originAses"]:
-   
-#   print("uniqueAs = {}".format(uniqueAses["originAs"]))
-#   print("paths = {}".format(uniqueAses["paths"]))
-   # print("paths = {}".format(uniqueAses["paths"]))
-#   print("#####")
-
-# write new updated paths to the filesystem        
-#write_to_filesystem(results)
